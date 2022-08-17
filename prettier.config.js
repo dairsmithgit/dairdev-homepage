9361eda71199654b9d8956f305b958d92cb8c03c
Dair Smith <dair.code@gmail.com>
2022-08-17 00:05:21 -0500

    initial commit, configs

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+const options = {
+    arrowParens: 'avoid',
+    singleQuote: true,
+    backetSpacing: true,
+    endOfLine: 'lf',
+    semi: false,
+    tabWdith: 2,
+    trailingComma: 'none'
+}
+
+module.exports = options
