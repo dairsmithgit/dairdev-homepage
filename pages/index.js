48e0aa14f513262e853471a90b5db465a0e11179
Dair Smith <dair.code@gmail.com>
2022-09-01 19:04:20 -0500

    add: add images for works. implement page transitions
    add: add all images for the works page and project detail photos. add page transitions by wrapping pages with layout component animated using framer motion animatePresence

--- pages/index.js
+++ pages/index.js
@@ -1,74 +1,77 @@
 import NextLink from "next/link";
 import { Button, Container, Box, Heading, Image, Link, useColorModeValue } from "@chakra-ui/react";
 import { ChevronRightIcon } from "@chakra-ui/icons";
+import Layout from "../components/layouts/article";
 import Section from "../components/Section";
 import Paragraph from "../components/Paragraph";
 import { BioSection, BioYear } from "../components/Bio";
 
 const Page = () => {
-    return <Container>
-        <Box borderRadius="lg" bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} p="3" mb="6" align="center">
-            Hello, I&apos;m a full-stack developer based in Texas!
-        </Box>
+    return <Layout>
+        <Container>
+            <Box borderRadius="lg" bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} p="3" mb="6" align="center">
+                Hello, I&apos;m a full-stack developer based in Texas!
+            </Box>
 
-        <Box display={{ md: 'flex' }}>
-            <Box flexGrow={1}>
-                <Heading as="h2" variant="page-title">
-                    Dair Smith
+            <Box display={{ md: 'flex' }}>
+                <Box flexGrow={1}>
+                    <Heading as="h2" variant="page-title">
+                        Dair Smith
+                    </Heading>
+                    <p>Living Digitally ( Developer / Designer / Artist )</p>
+                </Box>
+                <Box flexShrink={0} mt={{ base: 4, md: 0 }} ml={{ md: 6 }} align="center">
+                    <Image borderColor="whiteAlpha.800" borderWidth={2} borderStyle="solid" maxWidth="100px" display="inline-block" borderRadius="full" src="/images/dair1.png" alt="Profile Image" />
+                </Box>
+            </Box>
+            <Section delay={0.1}>
+                <Heading as="h3" variant="section-title">
+                    Work
                 </Heading>
-                <p>Living Digitally ( Developer / Designer / Artist )</p>
-            </Box>
-            <Box flexShrink={0} mt={{ base: 4, md: 0 }} ml={{ md: 6 }} align="center">
-                <Image borderColor="whiteAlpha.800" borderWidth={2} borderStyle="solid" maxWidth="100px" display="inline-block" borderRadius="full" src="/images/dair1.png" alt="Profile Image" />
-            </Box>
-        </Box>
-        <Section delay={0.1}>
-            <Heading as="h3" variant="section-title">
-                Work
-            </Heading>
-            <Paragraph>Dair is a full-stack web developer based in Austin, TX with a passion for building fun, modern web apps that inspire others. He has a keen eye for good design and enjoys learning new technology to ceaselessly improve his skills in the web world. When not online, he takes inspiration from music, photography, and the natural world to inspire his work. Dair is currently looking for full-time work as a web developer in front-end, back-end, or full-stack teams.</Paragraph>
-            <Box align="center" my={4}>
-                <NextLink href="/works">
-                    <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
-                        My Portfolio
-                    </Button>
-                </NextLink>
-            </Box>
-        </Section>
-        <Section delay={0.2}>
-            <Heading as="h3" variant="section-title" >
-                Bio
-            </Heading>
-            <BioSection>
-                <BioYear>1997</BioYear>
-                Born in Portland, Oregon.
-            </BioSection>
-            <BioSection>
-                <BioYear>2019</BioYear>
-                hello world! Wrote first line of code.
-            </BioSection>
-            <BioSection>
-                <BioYear>2021</BioYear>
-                Completed full-stack web developer bootcamp at Grand Circus in Detroit, MI. Learning MERN stack, git, and more.
-            </BioSection>
-            <BioSection>
-                <BioYear>2022</BioYear>
-                Moved to Austin, TX.
-            </BioSection>
-            <BioSection>
-                <BioYear>Present</BioYear>
-                Learning everyday.
-            </BioSection>
-        </Section>
-        <Section delay={0.3}>
-            <Heading as="h3" variant="section-title">
-                I ♥
-            </Heading>
-            <Paragraph>
-                Art, Music, Gaming, Making Beats, Photography, Creative Coding
-            </Paragraph>
-        </Section>
-    </Container>
+                <Paragraph>Dair is a full-stack web developer based in Austin, TX with a passion for building fun, modern web apps that inspire others. He has a keen eye for good design and enjoys learning new technology to ceaselessly improve his skills in the web world. When not online, he takes inspiration from music, photography, and the natural world to inspire his work. Dair is currently looking for full-time work as a web developer in front-end, back-end, or full-stack teams.</Paragraph>
+                <Box align="center" my={4}>
+                    <NextLink href="/works">
+                        <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+                            My Portfolio
+                        </Button>
+                    </NextLink>
+                </Box>
+            </Section>
+            <Section delay={0.2}>
+                <Heading as="h3" variant="section-title" >
+                    Bio
+                </Heading>
+                <BioSection>
+                    <BioYear>1997</BioYear>
+                    Born in Portland, Oregon.
+                </BioSection>
+                <BioSection>
+                    <BioYear>2019</BioYear>
+                    hello world! Wrote first line of code.
+                </BioSection>
+                <BioSection>
+                    <BioYear>2021</BioYear>
+                    Completed full-stack web developer bootcamp at Grand Circus in Detroit, MI. Learning MERN stack, git, and more.
+                </BioSection>
+                <BioSection>
+                    <BioYear>2022</BioYear>
+                    Moved to Austin, TX.
+                </BioSection>
+                <BioSection>
+                    <BioYear>Present</BioYear>
+                    Learning everyday.
+                </BioSection>
+            </Section>
+            <Section delay={0.3}>
+                <Heading as="h3" variant="section-title">
+                    I ♥
+                </Heading>
+                <Paragraph>
+                    Art, Music, Gaming, Making Beats, Photography, Creative Coding
+                </Paragraph>
+            </Section>
+        </Container>
+    </Layout>
 }
 
 export default Page;
--- pages/works.js
+++ pages/works.js
@@ -1,28 +1,32 @@
 import { Container, Heading, SimpleGrid, Divider } from "@chakra-ui/react";
 import Section from "../components/Section";
 import { WorkGridItem } from "../components/GridItem";
+import Layout from "../components/layouts/article";
+
 import thumbInspo from '../public/images/works/inspo_eyecatch.png';
 import thumbInoue from '../public/images/works/inoue_eyecatch.png';
 
 const Works = () => {
     return (
-        <Container>
-            <Heading as="h3" fontSize={20} mb={4}>
-                Works
-            </Heading>
-            <SimpleGrid columns={[1, 1, 2]} gap={6}>
-                <Section>
-                    <WorkGridItem id="inspo" title="Inspo" thumbnail={thumbInspo}>
-                        A social-media-like feed for images that inspire and inform my work.
-                    </WorkGridItem>
-                </Section>
-                <Section>
-                    <WorkGridItem id="inouestation" title="Inoue Station" thumbnail={thumbInoue}>
-                        Google Chrome extension to view a dashbaord for the fictional "Toro Inoue Train Station".
-                    </WorkGridItem>
-                </Section>
-            </SimpleGrid>
-        </Container>
+        <Layout>
+            <Container>
+                <Heading as="h3" fontSize={20} mb={4}>
+                    Works
+                </Heading>
+                <SimpleGrid columns={[1, 1, 2]} gap={6}>
+                    <Section>
+                        <WorkGridItem id="inspo" title="Inspo" thumbnail={thumbInspo}>
+                            A social-media-like feed for images that inspire and inform my work.
+                        </WorkGridItem>
+                    </Section>
+                    <Section>
+                        <WorkGridItem id="inouestation" title="Inoue Station" thumbnail={thumbInoue}>
+                            Google Chrome extension to view a dashboard for the fictional "Toro Inoue Train Station".
+                        </WorkGridItem>
+                    </Section>
+                </SimpleGrid>
+            </Container>
+        </Layout>
     )
 }
 
